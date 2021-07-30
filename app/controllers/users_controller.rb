6b9edc5f1509174669c61184b57b29d3eec709c0
Mariusz Baworowski <mariusz5006@gmail.com>
2021-07-30 16:46:37 +0200

    Dodano edycje uzytkownika

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 require 'bcrypt'
 class UsersController < ApplicationController
   
+  before_action :find_user_from_params_id, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
 
+  def edit
+  end
+
   def create
-    @user = User.new(get_user)
+    @user = User.new(get_user_params)
     if @user.save
       flash[:notice] = "Pomyślnie zarejestrowano użytkownika"
       redirect_to root_path
@@ -15,8 +20,23 @@ class UsersController < ApplicationController
     end
   end
 
-  def get_user
+
+  def update
+    find_user_from_params_id
+    if @user.update(get_user_params)
+      flash[:notice] = "Pomyślnie zmodyfikowano dane użytkownika"
+      redirect_to edit_user_path
+    else
+      render 'edit'
+    end
+  end
+
+  def get_user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def find_user_from_params_id
+    @user = User.find(params[:id])
   end
 
 end
